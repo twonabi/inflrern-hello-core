fb10e151d1993da99b306cedb123394c65d19a08
jian <loveinansan1@gmail.com>
2024-10-24 18:22:47 +0900

    #23 inflearn-hello-core 스프링 핵심 원리 - 기본편
    class6_싱글톤 컨테이너_싱글톤 방식의 주의
    _2(문제 해결)
    
    실무에서 정말 많이 발생하는 문제이다.
    공유 필드는 항상 조심!!
    스프링 빈은 항상 무상태(stateless)로 설계하자!

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -1,6 +1,5 @@
 package hello.core.singleton;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,15 +14,15 @@ public class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A 사용자 10000원 주문
-        statefulService1.order("userA",10000);
+        int userAPrice = statefulService1.order("userA",10000);
         //ThreadB: B 사용자 20000원 주문
-        statefulService2.order("userB",20000);
+        int userBPrice = statefulService2.order("userB",20000);
 
         //ThreadA: A 사용자 주문 금액을 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+     //   int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+       // Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
 
     }
 
