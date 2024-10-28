67cc8cc55e98191f2a1dcdd02ab4898764a229d1
jian <loveinansan1@gmail.com>
2024-10-28 17:56:59 +0900

    #31 inflearn-hello-core 스프링 핵심 원리 - 기본편
    class8_의존관계 자동 주입_조회 빈이 2개 이상_문제 & 해결
    
    해결 방법))
    - @Autowired 필드 명 매칭
    - @Qualifier -> @Qualifier끼리 매칭 -> 빈 이름 매칭
    - @Primary 사용
    
    @Qualifier ))
    @Qualifier 는 추가 구분자를 붙여주는 방법이다. 주입시 추가적인 방법을 제공하는 것이지 빈 이름을 변경하는 것은 아니다.
    - 주입시에 @Qualifier 를 붙여주고 등록한 이름을 적어준다.
    예))
     @Autowired
         public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
      -----------------
        @Component
        @Qualifier("mainDiscountPolicy")
        public class RateDiscountPolicy implements DiscountPolicy {
    
    @Primary ))
    우선순위를 정하는 방법이다. @Autowired시에 여러 빈이 매칭되면 @Primary 가 우선권을 가진다.
    (조금 더 깔끔하게 사용가능해서 많이 사용된다.)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,17 +3,22 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component
-@RequiredArgsConstructor
-public class OrderServiceImpl implements OrderService {
+
+public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -21,5 +26,10 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
