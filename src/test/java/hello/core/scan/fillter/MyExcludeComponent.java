e0da08d710071fb6bc90ef3603b52a60fab96252
jian <loveinansan1@gmail.com>
2024-10-28 14:27:27 +0900

    #27 inflearn-hello-core 스프링 핵심 원리 - 기본편
    class7_컴포넌트 스캔_필터
    
     기본값이어서 생략 가능!!
     type = FilterType.ANNOTATION,

---
+++ src/test/java/hello/core/scan/fillter/MyExcludeComponent.java
@@ -0,0 +1,10 @@
+package hello.core.scan.fillter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+
+}
